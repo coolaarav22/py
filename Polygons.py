321066b6d32de7b630b4fdd5c73d884f8b568a2d
coolaarav22 <dkgill33@gmail.com>
2025-09-02 18:36:28 +0530

    Add files via upload

---
+++ Polygons.py
@@ -0,0 +1,20 @@
+import turtle
+turtle.Screen().bgcolor(green)
+board = turtle()
+#triangle
+board.forward(100)
+
+board.left(120)
+
+board.left(120)
+#square
+board.forward(100)
+
+board.forward(100)
+
+board.forward(100)
+board.left(99)
+
+board.forward(100)
+
+#
