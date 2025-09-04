34bc420a9fd116d0e0ec7a91f28a2392bf6bc2ed
coolaarav22 <dkgill33@gmail.com>
2025-09-04 18:42:51 +0530

    Add files via upload

---
+++ player 456.py
@@ -0,0 +1,11 @@
+number = int(input("ender number to check"))
+print("number to be checked;" , number)
+
+if number>0:
+ print("this is a positive number")
+
+elif number<0:
+ print("this is a nagetive number")
+
+else:
+ print("this is  neutral")
