ce77547124aa7767b870baa15e67892ad2633151
coolaarav22 <dkgill33@gmail.com>
2025-09-16 18:56:00 +0530

    Add files via upload

---
+++ part 2 of parrots.py
@@ -0,0 +1,8 @@
+class parrot:
+# instance attributs
+def _init_(self,name,age):
+  self . name = Name
+  self . age = age
+# instance method 
+def sing(self,song):
+return "sing . format (self.name,song)"
