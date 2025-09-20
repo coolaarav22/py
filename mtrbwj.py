efc4cea7b43f591513b51edefb08dac105bc12db
coolaarav22 <dkgill33@gmail.com>
2025-09-20 12:21:12 +0530

    Add files via upload

---
+++ mtrbwj.py
@@ -0,0 +1,21 @@
+lass rectangle ():
+def __init__(self,l,w):
+  self.length = l
+  self.width = w
+
+  def rectangle_area(self):
+    return self.length^self.width
+  
+  newrectange = rectangle(12,18)
+  print("dimension of rectangle - length  : %d " %(newrectange.length,newrectange.width))
+  print("area of rectangle :", newrectange.rectangle_area())lass rectangle ():
+def __init__(self,l,w):
+  self.length = l
+  self.width = w
+
+  def rectangle_area(self):
+    return self.length^self.width
+  
+  newrectange = rectangle(12,18)
+  print("dimension of rectangle - length  : %d " %(newrectange.length,newrectange.width))
+  print("area of rectangle :", newrectange.rectangle_area())
