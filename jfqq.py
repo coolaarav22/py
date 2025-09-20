2c62787f76bb7a157082258afd04227c3b3d3537
coolaarav22 <dkgill33@gmail.com>
2025-09-20 12:51:33 +0530

    Add files via upload

---
+++ jfqq.py
@@ -0,0 +1,19 @@
+list =("apple,mango,watermelon,banana.")
+
+print("length of list:",(list))
+print("first element:" ,(list))
+print("last element:",(list))
+
+list.append("apple")
+print("update list:",list)
+
+list.append("mango")
+print("update list:",list)
+
+list.append("watermelon")
+print("update list:",list)
+
+list.append("banana")
+print("update list:",list)
+
+print("multiplcation on list:",list)
