f84886a5366a94e617e9163ab5c49f224ca18f75
coolaarav22 <dkgill33@gmail.com>
2025-09-30 18:40:35 +0530

    Add files via upload

---
+++ phone.py
@@ -0,0 +1,24 @@
+import sys
+
+def initial__phonebook():
+
+rows,cols = int(input("please ender intial number of contacts")),5
+phone_book = []
+print('phone_book')
+
+
+
+for i in range(rows):
+ print("ender cotact details")
+ print("NOTE: * indicates mandatory fields")
+ print("........................................................")
+
+
+ for j in range(5):   # 0,1,2,3,4
+  if j == 1:
+    temp.append(str(input("ender number : ")))
+
+if j == 2:
+ if temp[j] ==  
+
+
