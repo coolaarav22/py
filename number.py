19a1c4cd55cea7307b23caae8588074cae7f8b26
coolaarav22 <dkgill33@gmail.com>
2025-10-01 15:48:58 +0530

    Add files via upload

--- number.py
+++ number.py
@@ -1,6 +1,42 @@
-num = 1
-sum = 0
-while (num <= 10):
-   sum = sum+num
-   num = num+1
-   print("sum of first 10 natral number:",sum )
+# empty dictionaries
+
+my_dict = []
+
+# dictionaries with integer keys
+
+my_dict = {1: 'apple',2: 'ball'}
+
+# dictionaries with mixed keys
+
+my_dict = {'name' : 'John', 1 : (2,3,4)}
+
+my_dict = {'name' : 'jack' 'age' '26'}
+
+# output : jack
+
+print(my_dict['name'])
+print(my_dict.get['age'])
+
+# update value
+
+my_dict['age'] = 27
+print(my_dict)
+
+# add item
+
+my_dict['address'] = 'downtown'
+print(my_dict)
+
+# remove particular  element
+
+my_dict.pop ('age')
+print(my_dict)
+
+# access a particutar element
+
+print('address :', my_dict.get('address'))
+
+#remove all the element
+
+my_dict.clear()
+print(my_dict)
