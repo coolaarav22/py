084b1685e429d2e8cd63d88379d5130fac6d98a5
coolaarav22 <dkgill33@gmail.com>
2025-10-09 18:57:08 +0530

    Add files via upload

--- removelines.py
+++ removelines.py
@@ -1,2 +1,18 @@
 file1 = open('codingal.txt','r')
 file2 = open('codlingupdated.txt','w')
+for line in file1.readlines():
+	
+	# reading all lines that do not
+	# begin with "Coding"
+	if not (line.startswith('Coding')):
+		
+		# printing those lines
+		print(line)
+		
+		# storing only those lines that
+		# do not begin with "Coding"
+		file2.write(line)
+
+
+file2.close()
+file1.close()
