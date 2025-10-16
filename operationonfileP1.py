b648f19305525337d9c5040862d705480dcf3c72
coolaarav22 <dkgill33@gmail.com>
2025-10-16 18:47:03 +0530

    Add files via upload

--- operationonfileP1.py
+++ operationonfileP1.py
@@ -1,17 +1,15 @@
 
-file = open("codingal txt", "r")
-print(file.read())
-file.close
+with open ('Codingal.txt', 'w') as file:
+ file.write("hi! I am  penguin I am 1 year old")
+ file.close
 
 
-file = open('codingal.txt' , 'r')
-print("\n read in parts \n")
-print(file.read(8))
-file.close
 
 
-file = open('codingal.txt','a')
-file.write("hi! I am  penguin I am 1 year old")
-file.close
-
-
+ with open ('Codingal.txt' , 'r') as file:
+  date = file.readlines()
+ print('world in this file are . . . .')
+for lines in date:
+ word = line.split()
+ print (word)
+ file.close()
